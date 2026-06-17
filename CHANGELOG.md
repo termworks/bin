6e985f22c848da0b4d24ccb6c488a74b2676c331
Trim Bresilla <trim.bresilla@gmail.com>
2026-06-17 21:15:09 +0200

    chore(release): prepare for 0.1.2

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## [0.1.2] - 2026-06-17
+
+### <!-- 0 -->⛰️  Features
+
+- Rewrite
+- Rewrite
+
--- src/main.go
+++ src/main.go
@@ -11,7 +11,7 @@ import (
 
 // nolint: gochecknoglobals
 var (
-	version = "0.1.1"
+	version = "0.1.2"
 	commit  = ""
 	date    = ""
 	builtBy = ""
